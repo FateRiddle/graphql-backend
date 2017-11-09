fbd882b96ad9686386525bd52fdf82cdfe546106
FateRiddle <fateriddle@gmail.com>
2017-11-09 11:50:00 +0800

    a

--- src/schema/index.js
+++ src/schema/index.js
@@ -1,51 +1,17 @@
 const { makeExecutableSchema } = require('graphql-tools')
 const resolvers = require('./resolvers')
 
+// Define your types here.
 const typeDefs = `
   type Link {
     id: ID!
     url: String!
     description: String!
-    postedBy: User
   }
 
   type Query {
-    allLinks: [Link!]!
-    allUsers: [User!]!
-  }
-
-  type Mutation {
-    createLink(url: String!, description: String!): Link
-    createVote(linkId: ID!): Vote
-    createUser(name: String!, authProvider: AuthProviderSignupData!): User
-    signinUser(email: AUTH_PROVIDER_EMAIL): SigninPayload!
-  }
-
-  type User {
-    id: ID!
-    name: String!
-    email: String
-  }
-
-  type Vote {
-    id: ID!
-    user: User!
-    link: Link!
-  }
-
-  type SigninPayload {
-    token: String
-    user: User
-  }
-
-  input AuthProviderSignupData {
-    email: AUTH_PROVIDER_EMAIL
-  }
-
-  input AUTH_PROVIDER_EMAIL {
-    email: String!
-    password: String!
+      allLinks: [Link!]!
   }
 `
-
+// Generate the schema object from your types definition.
 module.exports = makeExecutableSchema({ typeDefs, resolvers })
